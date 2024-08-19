a039b39da7a305141398babd367fe73a6a3d939b
pgodinez <pgodinez@segob.gob.mx>
2024-08-19 13:11:42 -0600

    var types

---
+++ 01_Intro/01_hello_world.dart
@@ -0,0 +1,10 @@
+void main(){
+  // var myName = 'myName';
+  // final myName = 'myName';
+  // late final myName = 'myName';
+  const myName = 'myName';
+
+  print('Hola $myName'); // Output: myName
+  print('Hola ${myName.toUpperCase()}'); 
+  print('Hola ${ 1+1 }'); 
+}
